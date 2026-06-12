38dc56ab3e0fa7d4b6dc2b32f7d3af7ce5c29b52
bani80225-prog <bani80225@gmail.com>
2026-06-12 15:25:33 +0530

    Add files via upload

---
+++ prototype_6/src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+	@RequestMapping("/portfolio")
+	public String portfolio() {
+		return "portfolio.html";
+	}
+}
